3b2fa447450d6cb9decd460f39da06caddd13fae
David Grimes <davidgrimes@Davids-MacBook-Pro.local>
2023-08-19 23:23:04 -0500

    adding read me

--- App_user_interface.py
+++ App_user_interface.py
@@ -1,3 +1,3 @@
 from podcast_graph import spahdefy
-genre_choice = input('What genre of podcasts would you like to listen to? Please type "Entertainment, Comedy, or Information" ')
+genre_choice = input('What genre of podcasts would you like to listen to? Please type "Entertainment, Comedy, or Information and then press Enter" ')
 spahdefy.grab_by_genre(genre_choice)
